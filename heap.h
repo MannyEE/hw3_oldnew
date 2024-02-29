a822e663c78e5381cac57a5a9ee2f746478b64bd
Emmanuel Ezirim <Eezirim23@gmail.com>
2024-02-29 14:30:50 -0800

    Failed heap stress test

--- heap.h
+++ heap.h
@@ -5,7 +5,7 @@
 #include <vector>
 
 template <typename T, typename PComparator = std::less<T> >
-class Heap : private std::vector
+class Heap : private std::vector<T>
 {
 public:
   /**
@@ -16,13 +16,13 @@ public:
    *          as an argument and returns a bool if the first argument has
    *          priority over the second.
    */
-  Heap(int m=2, PComparator c = PComparator());
+  Heap(int m=2, PComparator c = PComparator()) : comparer(c), d_ary(m) {}
 
   /**
   * @brief Destroy the Heap object
   * 
   */
-  ~Heap();
+  ~Heap(){}
 
   /**
    * @brief Push an item to the heap
@@ -61,14 +61,71 @@ public:
   size_t size() const;
 
 private:
-  /// Add whatever helper functions and data members you need below
-
-
-
+  /// Add whatever helper functions and data members you need below 
+    int d_ary;
+    PComparator comparer;
+    void heapify(int idx);
+    void trickleUp(int loc);
 
 };
 
+// template <typename T, typename PComparator>
+// Heap<T,PComparator>::Heap(int m=2, PComparator c = PComparator()){}
+
+// template <typename T, typename PComparator>
+// Heap<T,PComparator>::~Heap(){}
+
 // Add implementation of member functions here
+template <typename T, typename PComparator>
+void Heap<T,PComparator>::trickleUp(int loc)
+{
+    // could be implemented recursively
+    int parent = (loc - 1)/d_ary;
+    while(comparer(std::vector<T>::at(loc), std::vector<T>::at(parent))) { 
+        T temp = std::vector<T>::at(parent);
+        std::vector<T>::at(parent) = std::vector<T>::at(loc);
+        std::vector<T>::at(loc) = temp;
+
+        loc = parent;
+        parent = (loc - 1)/d_ary;
+    }
+
+
+}
+
+
+template <typename T, typename PComparator>
+void Heap<T,PComparator>::heapify(int idx)
+{
+    if(idx > std::vector<T>::size() / 2) return;
+
+    int bestChild = 2; // start w/ left
+    int nextChild = bestChild + 1;
+
+    while(/*nextChild % d_ary != 1 && */nextChild < std::vector<T>::size() && comparer(std::vector<T>::at(nextChild - 1), std::vector<T>::at(bestChild - 1))) {
+        bestChild = nextChild;
+        nextChild++;
+    }
+
+    // nextChild--;
+
+    if(comparer(std::vector<T>::at(bestChild - 1), std::vector<T>::at(idx - 1))){ // questionable
+        T temp = std::vector<T>::at(idx - 1);
+        std::vector<T>::at(idx - 1) =  std::vector<T>::at(bestChild - 1);
+        std::vector<T>::at(bestChild - 1) = temp;
+        heapify(bestChild);
+    }
+}
+
+template <typename T, typename PComparator>
+void Heap<T,PComparator>::push(const T& item) 
+{
+
+    std::vector<T>::push_back(item);
+    trickleUp(std::vector<T>::size()-1);
+    
+}
+
 template <typename T, typename PComparator>
 bool Heap<T,PComparator>::empty() const
 {
@@ -109,19 +166,19 @@ T const & Heap<T,PComparator>::top() const
 template <typename T, typename PComparator>
 void Heap<T,PComparator>::pop()
 {
-  if(empty()){
-    // ================================
-    // throw the appropriate exception
-    // ================================
-
-
-  }
-
-
+    if(empty()){
+        // ================================
+        // throw the appropriate exception
+        // ================================
+        throw std::underflow_error("Cannot pop empty list");
+    }
+    std::vector<T>::at(0) = std::vector<T>::at(std::vector<T>::size()-1);
+    std::vector<T>::pop_back();
+    heapify(1);
 
 }
 
 
 
-#endif
+#endif;
 
